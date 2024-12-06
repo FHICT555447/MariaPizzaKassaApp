9c43a2628a54888589e871185be37f6de97fb77c
oxey <lucoerlemans37@gmail.com>
2024-12-06 11:00:05 +0100

    add something to send messages back and forth ad infinitum

--- Program.cs
+++ Program.cs
@@ -3,6 +3,16 @@ namespace dotnet_pizza_protocol {
     {
         static void Main(string[] args)
         {
+            UdpSender sender = new("192.168.68.242", 8888);
+
+            // var expanded = new PizzaOrderExpanded(10, "Pepperoni", "Small", [
+            //     new ExpandedModification(ModificationType.Add, "Mushrooms")
+            // ]);
+            var minimized = new PizzaOrderMinimized(11, 7);
+
+            // sender.Send(expanded.Serialize());
+            sender.Send(minimized.Serialize());
+
             using (var udpListener = new UdpReceiver(9999))
             {
                 udpListener.OnDataReceived += ProcessReceivedData;
@@ -18,6 +28,14 @@ namespace dotnet_pizza_protocol {
             PizzaMessage m = PizzaMessage.Deserialize(bytes);
 
             Console.WriteLine(m.GetType());
+
+            UdpSender sender = new("192.168.68.242", 8888);
+
+            var maybeBytes = m.Serialize();
+            if (maybeBytes is not null) {
+                sender.Send(maybeBytes);
+            }
+
         }
     }
 }
